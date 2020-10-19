f38533ceb8302c70f7f6b7024bc6350b848d9d4c
michael.mansour256@yahoo.com <michael.mansour256@yahoo.com>
2020-10-19 16:27:32 +0200

    edit js file

--- js/app.js
+++ js/app.js
@@ -1,104 +1,88 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-// nav bar global var
-const nav =document.getElementById('navbar__list');
-// sections global var
-const sec =document.querySelectorAll('section'); 
+// global var 
+sections = document.querySelectorAll('section');
+navUnOrderList = document.getElementById('navbar__list');
 
 
-
-/**
- 
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-const navigationbuilder = function(){
-    let navs='';
-    // loop over sections in my page
-    sec.forEach(section =>{
-        const id =section.id;
-        const navdata=section.dataset.nav;
-        // add each section to nav bar with its data and link to its id as unorderd list
-        navs +=`<li><a class="menu__link" href="#${id}">${navdata}</a></li>`;
-    })
-    // append the nav elements to the nav bar with is navbar__list
-    nav.innerHTML = navs;
-};
-navigationbuilder();
-
-// Add class 'active' to section when near top of viewport
-
-// getting the size of element 
-const active =(section)=>{
-    let ret = Math.floor(section.getBoundingClientRect().top);
-    return ret;
-};
-// remove active from section thats out of our screen
-const deleteActive=(section)=>{
-    section.classList.remove('your-active-class');
-    section.style.cssText="linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%);";
-};
-// add active to section that is scroll in now
-const addActive= (flag,section)=>{
-    if(flag){
-        section.classList.add('your-active-class');
-        section.style.cssText="background-color:rgba(255,50,50,.2);";
-    }
+// build navgation bar dynamiclly from sections in html file
+function buildnav (){
+    const myDocFrag = document.createDocumentFragment();
     
-};
+    sections.forEach(element => {
+        const sectionID=element.id;
+        const data = element.dataset.nav;
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.innerText  = data;
+        a.href = '#'+sectionID;
+        a.setAttribute('data-link',sectionID);
+        
+        a.className='menu__link';
+        console.log(a);
+        li.append (a) ;
+        myDocFrag.appendChild(li);
+        
+        
+   
 
-// getting our decision method to give active to one of our sections
-const activation=()=>{
-    sec.forEach(section=>{
-        const size = active(section);
-
-         inveiwport=()=> size<150 && size >= -150;
-         // delete active class 
-         deleteActive(section);
-         // add it if inviewport is true
-         addActive(inveiwport(),section);
     });
+    navUnOrderList.appendChild(myDocFrag);
+    
+}
+// call build nav function
+buildnav();
 
+
+// options for the observer 
+const options = {
+    root: null,
+    threshold : 0,
+    rootMargin : "-300px"
 };
+let observer = new IntersectionObserver(
+    (entries, observer) => { 
+    entries.forEach(entry => {
+        // if entry leave the portview remove the active class
+        if(!entry.isIntersecting){
+            entry.target.classList.remove('your-active-class');
+        }
+        // if entry enter the portview add the active class
+        else{
+            entry.target.classList.add('your-active-class');
+        }
+
+       
+      });
+    }, 
+options);
+// make pbserve for each section in the page
+sections.forEach(element=>{
+    observer.observe(element);
+});
 
 
-window.addEventListener('scroll',activation);
-// Scroll to anchor ID using scrollTO event
-const scrollusingscrollTo=()=>{
-    // get all links in navbar
-    const navlinks=document.querySelectorAll('navbar__menu a');
-    navlinks.forEach(link=>{
-        //for each link add listener for clicking
-        link.addEventListener('click',()=>{
-            //scroll to the section has clicked
-            for ( index = 0; index < sec; index++) {
-                sec[index].addEventListener('click',sectionScroll(link));
-                
-            }
+
+
+
+// scroll effect in nav links 
+function scrollto(){
+   // get all linkes in nav
+    const navlinks=document.querySelectorAll('.menu__link');
+    // for each make click listener
+    navlinks.forEach(link => {
+        
+        link.addEventListener('click',function(event){
+            // prevent the defaults to make smooth scroll 
+            event.preventDefault();
+            const scrollItem=document.getElementById(link.getAttribute('data-link'));
+            //scroll into view and make the behavior smooth
+            scrollItem.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'end',
+                    inline: "start"
+            });
         });
-    });
-};
-scrollusingscrollTo();
-/**
- * End Main Functions
- * 
-*/
+        
+});
+}
+// call scroll function
+scrollto();
